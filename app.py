5d880607045ca9708d30939a2ec7bd0b49cd5001
Tushar Pareek <110514444+atusharpareek@users.noreply.github.com>
2026-06-09 16:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # ================================
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly as px
 
 st.title("📦 Shipping Route Efficiency Dashboard")
 
